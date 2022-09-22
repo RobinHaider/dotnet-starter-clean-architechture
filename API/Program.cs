1db9e1dcac2305e831dcb8e15279c483c639b992
Robin Haider <robinhaider69@gmail.com>
2022-09-22 14:30:40 +0600

    Exception middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
@@ -40,6 +41,7 @@ using (var scope = app.Services.CreateScope())
 
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
