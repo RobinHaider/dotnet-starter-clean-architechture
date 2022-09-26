39ffa308391fd4158f5219e4ea5cc54526a9ff40
Robin Haider <robinhaider69@gmail.com>
2022-09-26 11:40:03 +0600

    Infrustructure Project and UserAccessor Class

---
+++ Application/Interfaces/IUserAccessor.cs
@@ -0,0 +1,8 @@
+namespace Application.Interfaces
+{
+    public interface IUserAccessor
+    {
+        string GetUsername();
+        string GetUserId();
+    }
+}
---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,26 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        }
+
+        public string GetUsername()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
